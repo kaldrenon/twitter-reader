25efe953cfa0342392c067d47e01462946fb45da
Andrew Fallows <kaldrenon@gmail.com>
2016-03-19 12:38:22 -0400

    convert to express app, stealing skeleton from Facebook's react tutorial

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+// Initialize Twitter client
+var Twitter = require('twitter');
+var twitterKeys = JSON.parse(fs.readFileSync(path.join(__dirname, 'twitter.json')));
+var twitterClient = new Twitter(twitterKeys);
+
 var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 3001));
@@ -75,3 +80,45 @@ app.post('/api/comments', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
+
+var topics = [
+  '#MissionTech',
+  'Kingdom Builders',
+  'Church Technology',
+  'god and gaming',
+  'faith and technology'
+]
+
+function searchAll(topic_list) {
+  topic_list.forEach(function(topic) {
+    search(topic);
+  });
+}
+
+function search(query) {
+  var params = {q: query};
+
+  twitterClient.get('search/tweets', params, function(error, results, response) {
+    var tweets = results.statuses;
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    if (tweets.length > 0) {
+      console.log("\n###");
+      console.log("### " + params.q);
+      console.log("###");
+      tweets.forEach(function(tweet) {
+        console.log("  " + tweet.user.name + " (" + tweet.user.screen_name + ")");
+        console.log("    " + tweet.text.replace("\n", "\n    "));
+        console.log();
+      });
+    } else {
+      console.log("No results for " + params.q);
+    }
+  });
+}
+
+searchAll(topics);
